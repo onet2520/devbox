d6008ed6f48ccd111b239f1fed3946e6afb2bfb9
Teng Yi <teny@microsoft.com>
2021-07-23 19:55:18 -0700

    Set default editor/Add powerlevel10k

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5,6 +5,16 @@ sudo apt update
 echo "[devbox] Install Vim..."
 sudo apt-get install vim
 
-echo "[devbox] Install fuzzy finder..."
+echo "[devbox] Install Oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Set default editor..."
+echo "export EDITOR=/usr/bin/vim" >> ~/.zshrc
+echo "export VISUAL=/usr/bin/vim" >> ~/.zshrc
+
+echo "[devbox] Install Powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "[devbox] Install Fuzzy Finder..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
